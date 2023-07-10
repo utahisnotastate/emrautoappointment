05915f7552fe1d206b9a75f6fae438647a75bbaf
utahisnotastate <utahisnotastate@gmail.com>
2023-07-10 03:07:11 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import EMRAutoAppointment from "./emrautoappointment";
 import './App.css';
-
+//Developed by Utah Hans for lablab Google Vertex AI Hackathon
+// https://lablab.ai/event/google-vertex-ai-hackathon
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <EMRAutoAppointment />
     </div>
   );
 }
